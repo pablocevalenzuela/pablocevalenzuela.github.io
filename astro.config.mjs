89132e0b985ca3fee92d14189cab7db8e1575a3b
Pablo Valenzuela <pabloce.valenzuela@gmail.com>
2024-02-01 20:59:03 -0300

    solved fixs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    //site: 'https://pablocevalenzuela.github.io',
+    //base: '/pablocevalenzuela.github.io',
+});
